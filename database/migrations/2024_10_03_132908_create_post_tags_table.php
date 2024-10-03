03536eb659c5cd7c0c6c53f8f72af858f3f5affe
Gudsteinn Arnarson <gudsteinn.arnarson@dfo.no>
2024-10-03 16:31:27 +0200

    finished day 12, practicing relationships

---
+++ database/migrations/2024_10_03_132908_create_post_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('post_post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Post::class)->constrained()->cascadeOnDelete();
+            $table->
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_tags');
+    }
+};
